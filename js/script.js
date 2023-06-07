0f65c534732bd9180af54441c73069ce735035d5
michelecanini <studio.michelecanini@gmail.com>
2023-06-07 19:16:40 +0200

    [UPDATE] bonus done

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ createApp({
                 message: 'Hello vuejs!',
                 input_text:'',
                 html: '<h1>Il Framework Vuejs ci fa risparmiare tempo!</h1>',
-            }
-        }
+                image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--I4FWon9Y--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ll22z8mea106b4vjdjy7.jpeg',
+                }
+            }     
 }).mount('#app');                              //-> collegamento al dom
