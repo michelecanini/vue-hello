5697816a883f9bf04219ea7b9988b43fa1a5e035
michelecanini <studio.michelecanini@gmail.com>
2023-06-07 16:48:58 +0200

    [UPDATE] insert vue_js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// Problema: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const {createApp} = Vue;                       //-> variabile globale vue                        
+
+createApp({
+        data(){                                //-> definizione del data
+            return{                            //-> oggetto di restituzione
+                message: 'Hello vuejs!'
+            }
+        }
+}).mount('#app');                              //-> collegamento al dom
